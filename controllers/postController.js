e961a9079176302ef8a9febf348ca69700ab9b38
goku057 <aridoanah@gmail.com>
2021-08-14 18:10:12 +0600

    activeUser added

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,22 +1,22 @@
-let createpost = (req , res)=>
-{
+const {id} = require("../helpers/activeUser.js");
+
+let createpost = (req, res) => {
     let pageTitle = "CreatePost";
-    let data = 
+    let data =
     {
         pageTitle
     }
-    res.render("createpost.ejs" , {data});
+    res.render("createpost.ejs", { data });
 }
 
 
-let createpostUser = (req , res)=>
-{
+let createpostUser = (req, res) => {
     console.log(req.body);
 }
 
 
 
-module.exports = 
+module.exports =
 {
     createpost,
     createpostUser
