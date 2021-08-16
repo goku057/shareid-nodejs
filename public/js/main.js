4fe709d9e7037e93fed2c5923a1f5c77ca7570ac
Al-Amin <s.alaminkhan96@gmail.com>
2021-08-16 23:36:18 +0600

    change in navbar search form and add function for submit form

--- public/js/main.js
+++ public/js/main.js
@@ -7,3 +7,10 @@ function closeMenu (){
       document.querySelector(".sidebar").classList.remove("open");
     //   console.log(e.target.className);
 }
+// Function for submit button on search bar
+const searchForm = document.getElementById("searchForm");
+document.querySelector(".submitButton").addEventListener("click", function(){
+
+  searchForm.submit();
+
+});
