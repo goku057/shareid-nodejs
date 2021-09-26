8532e3fbef9638b1f8ffb3ec1ad14a0232b31eea
Nabid <azizulhakimnabid@gmail.com>
2021-09-26 22:37:34 +0600

    Dashboard works - Nabid

--- controllers/profileController/dashboardController.js
+++ controllers/profileController/dashboardController.js
@@ -5,42 +5,33 @@ const { id } = require("../../helpers/activeUser");
 
 let accountInfo = async (req, res) =>{
     let activeUser = id; 
-    let showUserInfo = await accountInfoModel.showInfo(activeUser);
-    // console.log(showUserInfo.length);
+    let dashboardData = await accountInfoModel.showInfo(activeUser);
+    // console.log(dashboardData.length);
     let pageTitle = "Account-Information";
     let data = {
         pageTitle,
-        showUserInfo
+        dashboardData
     }
-
     res.render("account-info.ejs", {data});
 }
 
-let info = async (req, res) =>{
-    
+let info = async (req, res) =>{    
     let FirstName = req.body.fName;
     let LastName = req.body.lName;
     let dob = req.body.dob;
     let phone = req.body.phone;
     let activeUser = id; 
     console.log(req.body);
-
     await accountInfoModel.createInfo(activeUser, FirstName, LastName, dob, phone);
     res.redirect("/account-info");
-    
 }
 
 let showInfo = async (req, res) =>{
-    
+
     let activeUser = id; 
     console.log(req.body);
-    let showUserInfo = await accountInfoModel.showInfo(activeUser);
-    console.log(showUserInfo);
-
-
-
-
-
+    let dashboardData = await accountInfoModel.showInfo(activeUser);
+    console.log(dashboardData);
     let pageTitle = "Account-Information";
     let data = {
         pageTitle
